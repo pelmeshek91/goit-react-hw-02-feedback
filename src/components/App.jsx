efa747bed0453dffb952ab1efe4e391c5a8195d7
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-10-06 19:14:06 +0100

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 // import { User } from "./User/User";
-import { Feedback } from "./Feedback";
+import { Feedback } from './Feedback';
 // import { users } from "data/users"
 // import { Section } from "./Section/Section";
 // import { Button } from './Button';
@@ -12,10 +12,37 @@ export class App extends Component {
     bad: 0,
   };
 
-  
-  // clickHandler = () => {
-  //   this.setState({ isListShown: true });
-  // };
+  countFeedback = e => {
+    this.setState(prevState => {
+      const keys = Object.keys(prevState);
+      const element = keys.filter(key => key === e.target.name);
+      return {
+        [e.target.name]: prevState[element] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    let total = 0;
+    total = good + neutral + bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    let positive = 0;
+    positive = Math.round((good / (good + neutral + bad)) * 100)
+      ? Math.round((good / (good + neutral + bad)) * 100)
+      : 0;
+    return positive;
+  };
+
+  // userDelete = (userId) => {
+  //   this.setState(prevState => ({
+  //     users: prevState.users.filter(user => user.id !== userId)
+  //   }))
+  // }
 
   // userDelete = (userId) => {
   //   this.setState(prevState => ({
@@ -40,12 +67,14 @@ export class App extends Component {
   // }
 
   render() {
-    
     return (
       <>
-        
-        <Feedback options={ this.state } />
-        
+        <Feedback
+          options={this.state}
+          countFeedback={this.countFeedback}
+          countTotalFeedback={this.countTotalFeedback}
+          countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
+        />
       </>
     );
   }
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,25 +1,37 @@
 //import PropTypes from 'prop-types';
 //import { Paragraf, Span } from './User.styled';
+//import { countGoodFeedback } from './App'
 
-export const Feedback = ({ options: { good, neutral, bad } }) => {
-   
-
-    return (
-        <>
-            <h2>Please leave feedback</h2>
-            <button>Good</button>
-            <button>Neutral</button>
-            <button>Bad</button>
-            <h2>Statistics</h2>
-            <ul>
-                <li>Good: {good}</li>
-                <li>Neutral: {neutral}</li>
-                <li>Bad: {bad}</li>
-            </ul>
-
-        </>
-    );
-}
+export const Feedback = ({
+  options: { good, neutral, bad },
+  countFeedback,
+  countTotalFeedback,
+  countPositiveFeedbackPercentage,
+}) => {
+  console.log(countTotalFeedback());
+  return (
+    <>
+      <h2>Please leave feedback</h2>
+      <button name="good" type="button" onClick={countFeedback}>
+        Good
+      </button>
+      <button name="neutral" type="button" onClick={countFeedback}>
+        Neutral
+      </button>
+      <button name="bad" type="button" onClick={countFeedback}>
+        Bad
+      </button>
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {countTotalFeedback()}</li>
+        <li>Positive feedback: {countPositiveFeedbackPercentage()}%</li>
+      </ul>
+    </>
+  );
+};
 
 // User.propTypes = {
 //     user: PropTypes.shape({
